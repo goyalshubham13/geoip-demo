b6f90db61cfa961e4d0e9e1c9e97d85e6a2a6798
Shubham Goyal <shubham.goyal@qlik.com>
2017-05-04 18:17:14 +0530

    no message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const requestIp = require('request-ip');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Geo IP', ip:requestIp.getClientIp(req) });
 });
 
 module.exports = router;
