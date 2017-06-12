4023f525dc6f5fcf651c8f6727d654951503d97c
Shubham Goyal <shubham.goyal@qlik.com>
2017-06-12 14:14:09 +0530

    ipware

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const requestIp = require('request-ip');
-var get_ip = require('ipware')().get_ip;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  /*var ip = (req.headers['x-forwarded-for'] ||
+  var ip = (req.headers['x-forwarded-for'] ||
      req.connection.remoteAddress ||
      req.socket.remoteAddress ||
      req.connection.socket.remoteAddress).split(",")[0];
-     console.log (req.ip);*/
-  var ip_info = get_ip(req);
-  console.log(ip_info);
-  res.render('index', { title: 'Geo IP', ip:ip_info.clientIp });
+     console.log (req.ip);
+  res.render('index', { title: 'Geo IP', ip:ip });
 });
 
 module.exports = router;
